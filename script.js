4f5d87ed2718bbf5baa32a7592d3c6c246b1441f
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-05-27 18:03:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function stringChop(str, size) {
   size = Number(size);
   if (isNaN(size) || size <= 0) return [];
 
-	let result=[];
+
 		for(let i=0;i<str.length;i+=size){
 		result.push(str.slice(i, i + size));
 	}
